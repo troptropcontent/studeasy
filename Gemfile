cad02dac31c483f0f9bd2703c623d45b98f3be4c
Tom Ecrepont <tomecrepont@gmail.com>
2022-04-09 10:37:51 +0200

    add some validations

--- Gemfile
+++ Gemfile
@@ -60,6 +60,9 @@ gem "sassc-rails"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+#active storage validation
+gem 'active_storage_validations'
+
 group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,4 +1,11 @@
 class Assessment < ApplicationRecord
   belongs_to :user
-  has_many_attached :document
+  has_many_attached :documents
+
+  validates :deadline, presence: true
+  validates :name, presence: true
+  validates :documents, attached: true,
+                        limit: { min: 1, max: 3 },
+                        content_type: ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+                        size: { between: 1.kilobyte..100.megabytes }
 end
