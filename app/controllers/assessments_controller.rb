fb222c2a1f16b4568cbed2164f5137570f456d0b
Tom Ecrepont <tomecrepont@gmail.com>
2022-04-09 10:39:41 +0200

    Set up create and inde endpoints

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -3,7 +3,25 @@ class AssessmentsController < ApplicationController
     load_and_authorize_resource
 
     def index
-        
+        @assessment = Assessment.new
+    end
+
+    # POST /tweets
+    def create
+        respond_to do |format|
+            if @assessment.save
+                @new_assessment = Assessment.new
+                format.turbo_stream 
+            else
+                format.turbo_stream { render turbo_stream: turbo_stream.replace(@assessment, partial: "assessments/shared/form", locals: { assessment: @assessment}) }
+            end
+        end
+    end
+
+    private
+
+    def assessment_params
+        params.require(:assessment).permit(:name, :deadline, documents: [])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   root "assessments#index"
-  resources :assessments, only:[:index] 
+  resources :assessments, only:[:index, :create] 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
