c595cba2ddc15aa52db557e32c14e14416110955
Tom Ecrepont <tomecrepont@gmail.com>
2022-04-15 19:08:00 +0200

    update ability and create quote controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# This is where all the queries regarding quote ressources will end
+class QuotesController < ApplicationController
+  # POST /assessments/:assessment_id/quote
+  def update; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
-  root "assessments#index"
-  resources :assessments, only:[:index, :create, :destroy] 
+  root 'assessments#index'
+  resources :assessments, only: %i[index create destroy] do
+    resource :quote, only: :update
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
