03e2c5ed40378e0d864c8d319e8ccbc33f6249c8
Tom Ecrepont <tomecrepont@gmail.com>
2022-05-03 08:39:56 +0200

    create an helper to extract data from records in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# holds all the helpers that can be accessed application wide
 module ApplicationHelper
+  def extract_data(record, methods)
+    methods = methods.split('.')
+    methods.inject(record) { |object, method| object.send(:try, method) }
+  end
 end
