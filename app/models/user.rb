461ec165f93dcbcd3c11075aba4e7cd895f08be5
Tom Ecrepont <tomecrepont@gmail.com>
2022-06-12 16:46:06 +0200

    reword devise views and allow role to be sent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  ALLOWED_ADMIN_EMAIL = %w[assessmentcopilot@gmail.com].freeze
+
   has_many :assessments
 
-  enum role: [ :student, :buddy, :admin ]
+  enum role: { student: 0, buddy: 1, admin: 2 }
+
+  validates :email, inclusion: { in: ALLOWED_ADMIN_EMAIL, message: '%{value} is not a valid admin email' }, if: :admin?
 end
